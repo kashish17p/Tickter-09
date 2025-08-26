66814e2f63a63fcb628b096a36e6715bdb257c42
premkumar0706 <premkumar902645@gmail.com>
2025-08-26 09:08:27 +0530

    added new feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Navbar from "./Components/Navbar";
-
+import TicketCard from "./Components/TicketCard";
 
 export default function Home() {
   return (
     <main>
-      <Navbar/>
+      <Navbar />
       <div className="p-6 text-center">
         <h1 className="text-3xl font-bold">Welcome to Bu.fi Tickter 🎫</h1>
       </div>
+      <TicketCard />
     </main>
   );
 }
+

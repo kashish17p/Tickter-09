7b747908c5b9bacf75c76b6cb1eabdecec1e9836
premkumar0706 <premkumar902645@gmail.com>
2025-08-29 15:30:41 +0530

    added changes into pages.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <div className="p-6 text-center">
-        <h1 className="text-3xl font-bold">Welcome to Bu.fi Tickter 🎫</h1>
+      <div className="p-6 text-left">
+        <h1 className="text-3xl font-bold">Upcoming Events </h1>
       </div>
    <UpcomingEvent />
     </main>

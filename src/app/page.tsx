53f425a72816e24bf083b663026a03117752bd3d
premkumar0706 <premkumar902645@gmail.com>
2025-09-01 18:48:20 +0530

    added changes in UpcomingEvent.jsx and page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import Navbar from "./Components/Navbar";
 import TicketCard from "./Components/TicketCard";
 import DestinationBar from "./Components/DestinationBar";
-
-
-
 import UpcomingEvent from "./Components/UpcomingEvent";
 export default function Home() {
   return (
@@ -12,9 +9,6 @@ export default function Home() {
       <div className="text-center">
         <DestinationBar />
       </div>
-      
-    
-      
       <div className="p-6 text-left">
         <h1 className="text-3xl font-bold">Upcoming Events </h1>
       </div>

deef9f046f5330d59cbcf9dbacb4218c4162f562
kashish17p <kasp13115@gmail.com>
2025-09-03 02:53:08 +0530

    updated navbar

--- src/app/Components/DestinationBar.jsx
+++ src/app/Components/DestinationBar.jsx
@@ -5,21 +5,17 @@ import { SlLocationPin } from "react-icons/sl";
 export default function DestinationBar() {
   return (
    
-    <div className="w-full bg-yellow-400 py-6 px-8 flex items-center justify-between rounded-md shadow-md">
+    <div className=" hidden md:flex w-full bg-yellow-400 py-6 px-8 flex items-center text-start justify-between rounded-md shadow-md">
         <div className="flex items-center space-x-4">
-          {/* <MapPin className="w-5 h-5 text-blue-600" /> */}
-          <SlLocationPin className="w-22 h-20 text-white mx-4" />
-          {/* <span className="text-3xl"></span> */}
-        
+          <SlLocationPin className="w-18 h-20 text-white mx-4" />
         <div>
          <h2 className="text-xl font-bold text-black">Bundelkhand University</h2>
-         <p className="text-black text-sm">
-            Bundelkhand campus Jhansi, near Shivaji nagar.......
+         <p className="text-black text-sm">Bundelkhand campus Jhansi, near Shivaji nagar 
          </p>
         </div>
         </div> 
       
-      <button className="bg-gray-800 text-white text-sm font-medium py-2 px-10 rounded-lg">Change</button>
+      <button className="bg-gray-800 text-white text-sm font-medium w-30 h-12 rounded-lg">Change</button>
       
     </div>
   );
--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,15 +1,19 @@
 "use client";
 import { useState } from "react";
 import { IoMdSearch } from "react-icons/io";
-import { FaRegUser,} from "react-icons/fa";
+import { FaRegUser} from "react-icons/fa";
 import LoginForm from "./loginForm";
 import SignupForm from "./SignupForm";
 import Modal from "./Modal";
 import Image from "next/image";
 import { BiEnvelope, BiLockAlt, BiUser } from "react-icons/bi";
+import { Menu, X } from "lucide-react";
+
+
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const [user, setUser] = useState(null);
   const [formType, setFormType] = useState("signin");
 
@@ -29,52 +33,56 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="flex justify-between items-center px-10 py-4  text-white shadow-md">
+    <nav className="flex justify-between items-center px-10 md:px-12 py-4  text-white shadow-md">
       <div className="flex items-center space-x-2 mb-1">
      <Image src='/logo.svg' alt="/logo" width={250} height={250}/>
 
       </div>
 
-      <div className="relative w-160">
-        <IoMdSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-yellow-500" size={24} />
-        <input
-          type="text"
-          placeholder="Search for events and activities"
-          className="w-full pl-55 pr-4 py-2 rounded-lg border border-gray-400 text-black text-base placeholder:text-sm font-bold placeholder:text-gray-400"
-        />
-      </div>
-
-      {/* Create Event Button */}
-      <div className="flex items-center gap-4">
-        <button className="bg-yellow-400  font-bold text-black px-4 py-2 rounded-lg">
-          Create Event
-        </button>
-      </div>
-
-      {/* Icons */}
-      <div className="flex items-center gap-8">
-        <div className="w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center border-yellow-400">
-          {/* <FaRegHeart size={28} className="text-yellow-400 cursor-pointer" /> */}
-          <Image src="/heart.svg" alt="Heart Icon" width={24} height={24} />
-          
-
-        </div>
-        
-        <div className="w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center border-yellow-400">
-          <FaRegUser
-            size={25}
-            className="text-yellow-400 cursor-pointer"
-            onClick={() => setIsOpen(!isOpen)}
+       <div className="relative w-160 hidden md:block">
+         <IoMdSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-yellow-500" size={24} />
+         <input
+            type="text"
+            placeholder="Search for events and activities"
+            className="w-full pl-55 pr-4 py-2 rounded-lg border border-gray-400 text-black text-base placeholder:text-sm font-bold placeholder:text-gray-400"
           />
-          
-        </div>
       </div>
       
-{isOpen && (
-  <Modal onClose={() => setIsOpen(false)}>
+
+       {/* Create Event Button */}
+       <div className="hidden md:flex items-center space-x-5">
+         <button className="bg-yellow-400 font-bold text-black px-8 py-2 rounded-lg">
+           Create Event
+         </button>
+       </div>  
+
+       {/* Icons */}
+        <div className="hidden md:flex items-center gap-8">
+         <div className="w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center border-yellow-400">
+          <Image src="/heart.svg" alt="Heart Icon" width={24} height={24} />
+         </div>
+        
+         <div className="w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center border-yellow-400">
+           <FaRegUser
+             size={25}
+             className="text-yellow-400 cursor-pointer"
+             onClick={() => setIsOpen(!isOpen)}
+           />
+          
+         </div>
+       </div>
+        
+         <div className="w-12 h-12 rounded-full bg-gray-100 flex items-center justify-center md:hidden">
+          <Image src="/account_circle.svg" alt="Account Icon" width={24} height={24} />
+         </div>
+            
+      
+
+ {isOpen && (
+   <Modal onClose={() => setIsOpen(false)}>
     {user ? (
-      // If logged in
-      <div className="text-center">
+        // If logged in
+       <div className="text-center">
         <h2 className="text-lg font-bold">
           Hello <span className="text-yellow-600">{user.name}</span>
         </h2>
@@ -206,7 +214,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
-
-
-
